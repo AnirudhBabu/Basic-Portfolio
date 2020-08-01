7be8db764a277aa90a4e63afdbb0c14e068ed91c
Anirudh Babu <anirudhbabu2001@gmail.com>
2020-07-31 20:01:44 -0400

    AJAX injection complete

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,21 +8,23 @@
 (function(){
     function highlightActiveLink() 
     {
+        //gets all the nav links and creates siteName that stores the name of the site to be compared to href
         let navAnchors = document.querySelectorAll("li a");
-        
+        let siteName = document.title.toLowerCase() + ".html";
+
         for (const anchor of navAnchors) 
         {
+            //special separation as about me page has index.html as it's site name
             if(document.title === "About me")
             { 
                 if(anchor.getAttribute("href") === "index.html")
                 {
                     anchor.parentElement.className = "nav-item active";
-                }                
+                }
             }
             else
             {
-                let toBeCompared = document.title.toLowerCase() + ".html";
-                if(anchor.getAttribute("href") === toBeCompared)
+                if(anchor.getAttribute("href") === siteName)
                 {
                     anchor.parentElement.className = "nav-item active";
                 }
@@ -32,115 +34,91 @@
     
     function setContent()
     {
+        //calls this function here to counter the asynchronous nature of JavaScript
         highlightActiveLink();
-        switch (document.title) {
-            case "About me":
-                AboutMeContent();
-                break;
-            case "Contact":
-                ContactContent();
-                break;
-            case "Projects":
-                ProjectsText();
-                break;
-            default:
-                break;
-        }
+
+        // step 1 - creates the XHR object
+        let XHR = new XMLHttpRequest();
+
+        // step 2 - sets the type of the message to be read as JSON
+        XHR.responseType = 'json';
+
+        // step 3 - configures the message
+        XHR.open("GET", "./Scripts/paragraphs.json");
+
+        // step 4 - Executes the request
+        XHR.send();
+
+        XHR.addEventListener("readystatechange", function(){
+            if((XHR.readyState === 4) && (XHR.status === 200))
+            {
+                //stores the JSON content of the AboutMe object
+                let Contents = XHR.response.myContent;
+
+                //calls the appropriate function to load contents based on the title of the page
+                switch (document.title) {
+                    case "About me":
+                        AboutMeContent(Contents[0]);
+                        break;
+                    case "Contact":
+                        ContactContent(Contents[1]);
+                        break;
+                    case "Projects":
+                        ProjectsContent(Contents[2]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+                
+        });
+
+        //Again, method call is placed here to counter the async nature of JavaScript
         loadFooter();
 
     }
-    function AboutMeContent()
+    function AboutMeContent(aboutMeContent)
     {
-        // step 1 - creates the XHR object
-        let XHR = new XMLHttpRequest();
-
-        // step 2 - sets the type of the message to be read as JSON
-        XHR.responseType = 'json';
-
-        // step 3 - configures the message
-        XHR.open("GET", "./Scripts/paragraphs.json");
-
-        // step 4 - Executes the request
-        XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
-                //stores the JSON content of the AboutMe object
-                let aboutMeContent = XHR.response.AboutMe;
-
-                //sets values from the read JSON appropriately
-                document.getElementsByTagName("h1")[0].innerText = aboutMeContent.heading;
-                document.getElementsByTagName("h2")[0].innerText = aboutMeContent.missionHeading;
-                document.getElementsByTagName("p")[0].innerText = aboutMeContent.introText;
-                document.getElementsByTagName("blockquote")[0].innerText = aboutMeContent.missionStatement;
-                document.getElementsByTagName("h2")[1].innerText = aboutMeContent.hobbiesHeading;
-                document.getElementsByClassName("fa-ul")[0].innerHTML = aboutMeContent.hobbies;
-                document.getElementsByTagName("h2")[2].innerText = aboutMeContent.interestsHeading;
-                document.getElementsByClassName("fa-ul")[1].innerHTML = aboutMeContent.interests;
-            }
-        });
+        //sets values from the read JSON array's first object appropriately  
+        document.getElementsByTagName("h1")[0].innerText = aboutMeContent.heading;
+        document.getElementsByTagName("h2")[0].innerText = aboutMeContent.missionHeading;
+        document.getElementsByTagName("p")[0].innerText = aboutMeContent.introText;
+        document.getElementsByTagName("blockquote")[0].innerText = aboutMeContent.missionStatement;
+        document.getElementsByTagName("h2")[1].innerText = aboutMeContent.hobbiesHeading;
+        document.getElementsByClassName("fa-ul")[0].innerHTML = aboutMeContent.hobbies;
+        document.getElementsByTagName("h2")[2].innerText = aboutMeContent.interestsHeading;
+        document.getElementsByClassName("fa-ul")[1].innerHTML = aboutMeContent.interests;
         
     }
-    function ContactContent()
+    function ContactContent(contactContent)
     {
-        // step 1 - creates the XHR object
-        let XHR = new XMLHttpRequest();
-
-        // step 2 - sets the type of the message to be read as JSON
-        XHR.responseType = 'json';
-
-        // step 3 - configures the message
-        XHR.open("GET", "./Scripts/paragraphs.json");
-
-        // step 4 - Executes the request
-        XHR.send();
-
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.readyState === 4) && (XHR.status === 200))
-            {
-                //stores the JSON content of the AboutMe object
-                let contactContent = XHR.response.Contact;
-
-                //sets values from the read JSON appropriately
-                document.getElementsByTagName("h1")[0].innerText = contactContent.heading;
-                document.getElementsByTagName("label")[0].innerText = contactContent.firstNameLabel;
-                document.getElementsByTagName("input")[0].setAttribute("placeholder", contactContent.firstNamePlaceholder);
-                document.getElementsByTagName("label")[1].innerText = contactContent.lastNameLabel;
-                document.getElementsByTagName("input")[1].setAttribute("placeholder", contactContent.lastNamePlaceholder);
-                document.getElementsByTagName("label")[2].innerText = contactContent.contactNumLabel;
-                document.getElementsByTagName("input")[2].setAttribute("placeholder", contactContent.contactNumPlaceholder);
-                document.getElementsByTagName("label")[3].innerText = contactContent.emailAddressLabel;
-                document.getElementsByTagName("input")[3].setAttribute("placeholder", contactContent.emailAddressPlaceholder);
-                document.getElementsByTagName("label")[4].innerText = contactContent.shortMessageLabel;
-                document.getElementsByTagName("textarea")[0].setAttribute("placeholder", contactContent.shortMessagePlaceholder);
-                document.getElementsByTagName("button")[1].innerHTML = contactContent.sendButton;
-                document.getElementsByTagName("button")[2].innerHTML = contactContent.resetButton;
-            }
-        });
+        //sets values from the read JSON array's second object appropriately
+        document.getElementsByTagName("h1")[0].innerText = contactContent.heading;
+        document.getElementsByTagName("label")[0].innerText = contactContent.firstNameLabel;
+        document.getElementsByTagName("input")[0].setAttribute("placeholder", contactContent.firstNamePlaceholder);
+        document.getElementsByTagName("label")[1].innerText = contactContent.lastNameLabel;
+        document.getElementsByTagName("input")[1].setAttribute("placeholder", contactContent.lastNamePlaceholder);
+        document.getElementsByTagName("label")[2].innerText = contactContent.contactNumLabel;
+        document.getElementsByTagName("input")[2].setAttribute("placeholder", contactContent.contactNumPlaceholder);
+        document.getElementsByTagName("label")[3].innerText = contactContent.emailAddressLabel;
+        document.getElementsByTagName("input")[3].setAttribute("placeholder", contactContent.emailAddressPlaceholder);
+        document.getElementsByTagName("label")[4].innerText = contactContent.shortMessageLabel;
+        document.getElementsByTagName("textarea")[0].setAttribute("placeholder", contactContent.shortMessagePlaceholder);
+        document.getElementsByTagName("button")[1].innerHTML = contactContent.sendButton;
+        document.getElementsByTagName("button")[2].innerHTML = contactContent.resetButton;
         
     }
-    function ProjectsText()
+    function ProjectsContent(projectsContent)
     {
-        let project1String = 
-        `One of my favourite activities is to do these artwork which is done by making 3D triangular
-        blocks of paper and then making a base upon which the desired structure is built. This work
-        makes me feel rested and calm and I enjoy spending hours with it.`;
-        let project2String = 
-        `I enjoy participating in quiz competitions and have participated in many to my best.
-        Participation in quizzes always pays off, whether you win or lose and that's something
-         I have come to adore about these competitions.`;
-         let project3String = 
-        `I also enjoy travelling. I was born and brought up in Saudi Arabia for the whole of my life.
-         I have travelled to various parts of India including Dhanushkodi, Rameswaram and Madurai.`
-        document.getElementsByTagName("h1")[0].innerText = "Projects";
-        document.getElementsByTagName("h2")[0].innerText = "3D Origami";
-        document.getElementsByTagName("p")[0].innerText = project1String;
-        document.getElementsByTagName("h2")[1].innerText = "Quiz competitions";
-        document.getElementsByTagName("p")[1].innerText = project2String;
-        document.getElementsByTagName("h2")[2].innerText = "Travel";
-        document.getElementsByTagName("p")[2].innerText = project3String;
-        document.getElementsByTagName("h5")[0].innerHTML = "&copy; Copyright 2020 - Anirudh Babu - Centennial College"; 
+        //sets values from the read JSON array's third object appropriately
+        document.getElementsByTagName("h1")[0].innerText = projectsContent.heading;
+        document.getElementsByTagName("h2")[0].innerText = projectsContent.project1Heading;
+        document.getElementsByTagName("p")[0].innerText = projectsContent.project1Text;
+        document.getElementsByTagName("h2")[1].innerText = projectsContent.project2Heading;
+        document.getElementsByTagName("p")[1].innerText = projectsContent.project2Text;
+        document.getElementsByTagName("h2")[2].innerText = projectsContent.project3Heading;
+        document.getElementsByTagName("p")[2].innerText = projectsContent.project3Text;
+ 
     }
     function loadHeader()
     {
